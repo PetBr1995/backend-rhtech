d39b920ef3c179b450c4c190e07c6e74e31a7685
PetBr1995 <dev.petersonbrito@gmail.com>
2026-04-06 13:55:14 -0400

    Ajustes para deploy

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-﻿from flask import Flask, jsonify, request
+﻿import os
+
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -193,4 +195,6 @@ def criar_candidato():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv("PORT", "5000"))
+    debug = os.getenv("FLASK_DEBUG", "false").lower() == "true"
+    app.run(host="0.0.0.0", port=port, debug=debug)
